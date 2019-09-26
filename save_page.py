73ec185855ff21929a22f759fc552333036a4f26
lukasz-f <lukaszf6@gmail.com>
2019-09-26 11:59:36 +0200

    packt downloader

--- save_page.py
+++ save_page.py
@@ -2,7 +2,7 @@ import pyautogui
 import time
 import re
 
-pattern = r'9781788995573/.*$'
+pattern = r'9781786464453/.*$'
 
 print('start')
 time.sleep(5)
@@ -12,14 +12,14 @@ with open('save_url.txt', 'r') as f:
         print(url)
         pyautogui.hotkey('command', 'l')
         time.sleep(1)
-        pyautogui.typewrite(url + '\n')
+        pyautogui.typewrite(url + '\n', interval=0.1)
         time.sleep(10)
         pyautogui.hotkey('command', 's')
         time.sleep(1)
         pyautogui.press('left')
         time.sleep(1)
         file_name = re.findall(pattern, url)[0] + '/'
-        pyautogui.typewrite(file_name)
+        pyautogui.typewrite(file_name, interval=0.1)
         pyautogui.hotkey('enter')
         time.sleep(10)
 
--- save_url.py
+++ save_url.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 import time
 
-url = 'https://subscription.packtpub.com/book/application_development/9781788995573/1'
+url = 'https://subscription.packtpub.com/book/big_data_and_business_intelligence/9781786464453/1'
 driver = webdriver.Chrome('./chromedriver')
 time.sleep(5)
 
-with open('save_url.txt', 'w') as f:
+with open('save_url.txt', 'a') as f:
     while True:
         f.write(url)
         f.write('\n')
