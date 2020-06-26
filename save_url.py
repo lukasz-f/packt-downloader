af0af6026ea8c1b5b286db07260077cce09cef05
lukasz f <lukasz@MacBook-lukasz.local>
2020-06-26 20:39:19 +0200

    add new chromedriver to save_url script

--- save_url.py
+++ save_url.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import time
 
 url = 'https://subscription.packtpub.com/book/big_data_and_business_intelligence/9781786464453/1'
-driver = webdriver.Chrome('./chromedriver')
+driver = webdriver.Chrome('./chromedriver-83')
 time.sleep(5)
 
 with open('save_url.txt', 'a') as f:
